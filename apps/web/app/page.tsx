2022d526d3a7c1f1176faf2d0867ff48ab0298d1
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2025-12-15 17:51:08 +0530

    main branch

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,12 +2,12 @@ import {prismaClient} from 'db/client'
 
 export default async function Home() {
   
-  const users=await prismaClient.user.findMany()
+  // const users=await prismaClient.user.findMany()
   
 
   return (
     <>
-      {
+      {/* {
         users.map((item)=>(
           <div key={item.id} style={{padding:"5px", margin:"10px", display:"flex", gap:"10vh"}}>
             <div> {item.id}</div>
@@ -15,7 +15,9 @@ export default async function Home() {
             <div>{item.password}</div>
           </div>
         ))
-      }
+      } */}
+
+      <div> Hello, World! from main branch </div>
     </>
   );
 }
